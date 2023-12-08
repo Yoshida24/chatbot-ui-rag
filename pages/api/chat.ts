d1051f1ef5114c18b78b934765c40948901939af
Yoshida24 <trihead1.dev@gmail.com>
2023-12-08 20:21:52 +0900

    feat: RAGを追加

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,12 +1,11 @@
-import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE } from '@/utils/app/const';
-import { OpenAIError } from '@/utils/server';
+import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE, OPENAI_API_HOST } from '@/utils/app/const';
+import { OpenAIError, OpenAIStream } from '@/utils/server';
 import { Pinecone } from '@pinecone-database/pinecone';
 
 import { ChatBody, Message } from '@/types/chat';
 
 // @ts-expect-error
 import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
-
 import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
 import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
 
@@ -20,7 +19,6 @@ const indexName = process.env.PINECONE_INDEX;
 const embeddingmodel = process.env.DEFAULT_MODEL_EMBEDDING;
 
 async function getEmbedding(query: string, openaiKey: string) {
-  console.log({ apiKey, environment, indexName, embeddingmodel });
   try {
     const endpoint = 'https://api.openai.com/v1/embeddings';
     const response = await fetch(
@@ -38,7 +36,6 @@ async function getEmbedding(query: string, openaiKey: string) {
       }
     );
     const result = await response.json();
-    console.log(result);
     return result;
   } catch (error) {
     console.error('Error fetching embedding:', error);
@@ -85,13 +82,18 @@ const handler = async (req: Request): Promise<Response> => {
 
     encoding.free();
 
-    const query = messages[messages.length - 1].content;
+    const userMessage = messages[messages.length - 1];
+    const query = encodeURIComponent(userMessage.content.trim());
 
     // Transform query to vector
     const xqRaw = await getEmbedding(query, key);
     const xq = xqRaw?.data[0]?.embedding;
     if (xq == null) {
       console.error('Error fetching embedding:', xqRaw);
+      return new Response('Error', { status: 500 });
+    }
+
+    if (apiKey === undefined || environment === undefined || indexName === undefined) {
       return new Response('Error', { status: 500 });
     }
 
@@ -118,10 +120,41 @@ const handler = async (req: Request): Promise<Response> => {
     const formatted_top_info = `【${top_info.title}】
     ${top_info.summary}
     (Score: ${top_info.score})`
-    return new Response(formatted_top_info, { status: 200 });
 
-    // const stream = await OpenAIStream(model, promptToSend, temperatureToUse, key, messagesToSend);
-    // return new Response(stream);
+    const RAGAnswerMessage: Message = { role: 'assistant', content: formatted_top_info };
+    console.log(formatted_top_info)
+
+    const answerRes = await fetch(`${OPENAI_API_HOST}/v1/chat/completions`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${key ? key : process.env.OPENAI_API_KEY}`,
+        ...(process.env.OPENAI_ORGANIZATION && {
+          'OpenAI-Organization': process.env.OPENAI_ORGANIZATION,
+        }),
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        model: model.id,
+        messages: [
+          {
+            role: 'system',
+            content: `あなたは最高の映画評論家です。ユーザーからの映画についての質問に答えてください。
+            回答はマークダウン形式で記述してください。`,
+          },
+          RAGAnswerMessage,
+          userMessage,
+        ],
+        max_tokens: 2048,
+        temperature: 1,
+        stream: false,
+      }),
+    });
+
+    const secondRes = await answerRes.json();
+    console.log(secondRes)
+    const answerToUser = secondRes.choices[0].message.content;
+
+    return new Response(answerToUser);
   } catch (error) {
     console.error(error);
     if (error instanceof OpenAIError) {
